288d65e6380085209a83d24d467862d4d9515986
guillermo <guillermo.vilbar.1@educa.jcyl.es>
2025-12-02 18:48:19 +0100

    02/12

--- EjemplosBasicos/src/basicos/Del1al100.java
+++ EjemplosBasicos/src/basicos/Del1al100.java
@@ -14,9 +14,10 @@ public class Del1al100 {
 			System.out.println("");
 			
 		}
-			int contador = 1;
-			for ( int i =1; i <=10; i++) {
-				for (int j =1; j <=10; j++) {
+		 	
+			int contador = 
+			for ( int i = 0; i <=10; i++) {
+				for (int j =0; j <=10; j++) {
 					System.out.print( contador + " " );
 					contador++;}
 				System.out.println("");
